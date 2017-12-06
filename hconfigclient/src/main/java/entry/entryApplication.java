7a94ea3f848c259544121a617e816a71f6ecfc84
ly <ly@192.168.0.103>
2017-12-07 02:27:15 +0800

    eurekaServer

--- hconfigclient/src/main/java/entry/entryApplication.java
+++ hconfigclient/src/main/java/entry/entryApplication.java
@@ -3,11 +3,13 @@ package entry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient  //激活eureka中的DiscoveryClient实现
 public class entryApplication {
     public static void main(String[] args) {
         SpringApplication.run(entryApplication.class, args);
